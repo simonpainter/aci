f465fffec6c5f1588ba90cba486aa67ae33b11eb
Painter <Simon.Painter@leeds.gov.uk>
2020-12-02 21:05:04 +0000

    Added basic capability to create tenants, vrfs and bridge domains.

--- aci.py
+++ aci.py
@@ -19,8 +19,41 @@ def login(apic_ip,apic_username,apic_password):
     cookies['APIC-Cookie'] = login_attributes['token']
     return cookies
 
-def CEp(apic_ip, cookies):
+def fvCEp(apic_ip, cookies):
     request_url = '/node/class/fvCEp.json'
     base_url = 'https://%s/api/' % apic_ip
     response_data = requests.get(base_url + request_url, cookies=cookies, verify = False)
     return response_data.json()
+
+def fvTenant(apic_ip, cookies):
+    request_url = '/node/class/fvTenant.json'
+    base_url = 'https://%s/api/' % apic_ip
+    response_data = requests.get(base_url + request_url, cookies=cookies, verify = False)
+    return response_data.json()
+
+def postObject(apic_ip, cookies, objectData):
+    request_url = '/mo/uni.json'
+    base_url = 'https://%s/api/' % apic_ip
+    response_data = requests.post(base_url + request_url, cookies=cookies, json = objectData, verify = False)
+    return response_data.json()
+
+def createTenant(apic_ip, cookies, name):
+    jsonData = '{"fvTenant" : {"attributes" : {"name" : "%s"}}}' % name
+    objectData = json.loads(jsonData)
+    return postObject(apic_ip, cookies, objectData)
+
+def deleteTenant(apic_ip, cookies, name):
+    jsonData = '{"fvTenant" : {"attributes" : {"name" : "%s", "status" : "deleted"}}}' % name
+    objectData = json.loads(jsonData)
+    return postObject(apic_ip, cookies, objectData)
+
+def createVrf(apic_ip, cookies, name, fvTenant):
+    jsonData = '{"fvCtx": {"attributes": {"dn": "uni/tn-%s/ctx-%s","name": "%s","rn": "ctx-%s","status": "created"}}}' % (fvTenant,name, name, name)
+    objectData = json.loads(jsonData)
+    return postObject(apic_ip, cookies, objectData)
+
+def createBD(apic_ip, cookies, name, fvCtx, fvTenant):
+    jsonData =  '{"fvBD": {"attributes": {"dn": "uni/tn-%s/BD-%s","name": "%s","status": "created"},"children": [{"fvRsCtx": {"attributes": {"tnFvCtxName": "%s"}}}]}}' % (fvTenant,name,name,fvCtx)
+    print(jsonData)
+    objectData = json.loads(jsonData)
+    return postObject(apic_ip, cookies, objectData)
